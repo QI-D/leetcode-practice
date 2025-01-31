d6bae4df4e19343efb3fef809f49f6c30dce4973
QI-D <qid14@outlook.com>
2025-01-30 23:18:09 -0800

    binary tree zigzag level order traversal
    Signed-off-by: QI-D <qid14@outlook.com>

---
+++ binary_tree_zigzag_level_order_traversal.py
@@ -0,0 +1,84 @@
+'''
+103. Binary Tree Zigzag Level Order Traversal
+Medium
+Topics
+Companies
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[20,9],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+'''
+from collections import deque
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        
+        result = []
+        queue = deque([root])
+        left_to_right = True
+
+        while queue:
+            level_size = len(queue)
+            level_nodes = []
+
+            for i in range(level_size):
+                node = queue.popleft()
+                level_nodes.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            if not left_to_right:
+                level_nodes.reverse()
+
+            result.append(level_nodes)
+            left_to_right = not left_to_right
+
+        return result
+
+
+
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+# Call the zigzagLevelOrder function
+result = Solution().zigzagLevelOrder(root)
+
+# Print the result
+print(result)
