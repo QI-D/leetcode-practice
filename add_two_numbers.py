213c94d8d6cd645d18762899735658f6cf7cbb0b
QI-D <qid14@outlook.com>
2025-02-13 15:06:58 -0800

    add two numbers
    Signed-off-by: QI-D <qid14@outlook.com>

---
+++ add_two_numbers.py
@@ -0,0 +1,99 @@
+'''
+2. Add Two Numbers
+Medium
+Topics
+Companies
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+ 
+
+Example 1:
+
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+'''
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def __repr__(self):
+        # A helper method for easy visualization
+        nodes = []
+        current = self
+        while current:
+            nodes.append(str(current.val))
+            current = current.next
+        return " -> ".join(nodes)
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode()
+        result = dummy
+
+        sum =  0
+        carry = 0
+
+        while l1 or l2 or carry:
+            sum = carry
+
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+
+            num = sum % 10
+            carry = sum // 10
+            dummy.next = ListNode(num)
+            dummy = dummy.next
+
+        return result.next
+
+
+
+def create_linked_list(arr):
+    if not arr:
+        return None
+    head = ListNode(arr[0])
+    current = head
+    for value in arr[1:]:
+        current.next = ListNode(value)
+        current = current.next
+    return head
+
+list1 = [2,4,3]
+list2 = [5,6,4]
+
+l1 = create_linked_list(list1)
+l2 = create_linked_list(list2)
+
+print(Solution().addTwoNumbers(l1, l2))
